7dcbe10847bff3d5ab2180828c5bd479b6f6e118
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-04-30 21:48:50 +0300

    Create component skeletons for PlotPage and PlotGrid components

---
+++ pb-client/src/components/PlotGrid.js
@@ -0,0 +1,10 @@
+function PlotGrid(props){
+    return(
+        <div>
+            <h2>This is a PlotGrid Component</h2>
+            <p>It will contain Plot elements</p>
+        </div>
+    )
+}
+
+export default PlotGrid;
---
+++ pb-client/src/components/PlotPage.js
@@ -0,0 +1,12 @@
+function PlotPage(props) {
+    return (
+        <div>
+            <h1>This is the PlotPage Component.</h1>
+            <p>You're working on {props.story}</p>
+            <p>It will be a top level component for certain pages</p>
+            <p>It will have a PlotGrid component</p>
+        </div>
+    )
+}
+
+export default PlotPage;
