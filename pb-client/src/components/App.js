ae994d89962473abd89e65296348bae78038e47e
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-04-30 21:48:01 +0300

    Run create-react-app and set up initial files

---
+++ pb-client/src/components/App.js
@@ -0,0 +1,12 @@
+import PlotPage from './PlotPage';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PlotPage story="Monte Cristo"></PlotPage>
+    </div>
+  );
+}
+
+export default App;
