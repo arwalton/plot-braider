a4470f1b429e4d843af1ee5afd0b2186cac2dffd
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-05-02 09:28:16 +0300

    Add skeleton of PlotHeader component

--- pb-client/src/components/Plot.js
+++ pb-client/src/components/Plot.js
@@ -1,9 +1,12 @@
+import PlotHeader from "./PlotHeader";
+
 function Plot(props){
     return (
         <div>
             <h3>This is a Plot component.</h3>
             <p>It will contain a single PlotHeader.</p>
             <p>It will have multiple PlotEvents</p>
+            <PlotHeader />
         </div>
     )
 }
---
+++ pb-client/src/components/PlotHeader.js
@@ -0,0 +1,10 @@
+function PlotHeader(plot){
+    return(
+        <div>
+            <h3>This is a PlotHeader component</h3>
+            <p>It will display the title of a particular subplot</p>
+        </div>
+    )
+}
+
+export default PlotHeader;
