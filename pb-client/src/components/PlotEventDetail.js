211d27580c83131fdc32c90eb339a4525f3b5e06
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-05-05 12:32:16 +0300

    Allow text box to be toggled between readOnly and editable

--- pb-client/src/components/PlotEventDetail.js
+++ pb-client/src/components/PlotEventDetail.js
@@ -1,18 +1,25 @@
+import { useState } from 'react';
+
 function PlotEventDetail(props){
-  let isReadOnly = true;
+  
+  const [isReadOnly, setIsReadOnly] = useState(true);
+
   return(
     <div>
       <h3>This is a PlotEventDetail</h3>
-      <form>
-        <label htmlFor="plot-event-text">{ props.title }</label>
-        <textarea id="plot-event-text"
-                  name="plot-event-text"
-                  cols="200"
-                  rows="20"
-                  readOnly={isReadOnly}
-                  placeholder="Describe this plot event in more detail">
-        </textarea>
-      </form>
+      <label htmlFor="plot-event-text">{ props.title }</label>
+      <textarea id="plot-event-text"
+                name="plot-event-text"
+                cols="200"
+                rows="20"
+                readOnly={isReadOnly}
+                placeholder="Describe this plot event in more detail">
+      </textarea>
+      <button onClick={()=>{
+          setIsReadOnly(!isReadOnly);
+          console.log(isReadOnly)}}>
+        {isReadOnly ? "Unlock" : "Lock"}
+      </button>
     </div>
   )
 }
